cf4b65d56edbad75f4fefc713ec577a52011fe82
Aakash Singh <aakashsingh717@gmail.com>
2024-12-22 00:35:07 -0500

    argument changes

--- backend/controllers/memberControllers.js
+++ backend/controllers/memberControllers.js
@@ -14,10 +14,10 @@ import {
 } from 'firebase/firestore';
 
 
-export const createMember = async (req,res,next)=>{
+export const createMember = async (request,response,next)=>{
     try{
-        const data = req.body;
-        const member_photo = req.files[0];
+        const data = request.body;
+        const member_photo = request.files[0];
 
         const storageRef = ref(fireStorageDB, `members/${data.name}_${Date.now()}_${member_photo.originalname}`);
 
@@ -29,19 +29,19 @@ export const createMember = async (req,res,next)=>{
         data.photoURL = photoURL
         delete(data.photo) 
         const docRef = await addDoc(collection(fireStoreDB, "members"),data);
-        res.status(200).send('member created sucessfully');
+        response.status(200).send('member created sucessfully');
     }catch(error){
-        res.status(400).send(error.message)
+        response.status(400).send(error.message)
     }
 }
 
-export const getMembers = async (req,res,next)=>{
+export const getMembers = async (request,response,next)=>{
     try{
 
         const members = await getDocs(collection(fireStoreDB, 'members'));
         const memberArray = [];
         if (members.empty){
-            res.status(400).send('No members found');
+            response.status(400).send('No members found');
         }else{
             members.forEach((doc)=>{
                 const data = doc.data()
@@ -58,21 +58,21 @@ export const getMembers = async (req,res,next)=>{
                 memberArray.push(member)
             });
 
-            res.status(200).send(memberArray)
+            response.status(200).send(memberArray)
         }
 
     }catch (error){
-        res.status(400).send(error.message)
+        response.status(400).send(error.message)
     }
 }
 
-export const deleteMember = async (req,res,next)=>{
+export const deleteMember = async (request,response,next)=>{
     try{
 
-        const memberId = req.body.id;
+        const memberId = request.body.id;
 
         if (!memberId){
-            res.status(400).send("ID not found!");
+            response.status(400).send("ID not found!");
         }
 
         const memberDoc = doc(fireStoreDB, "members", memberId);
@@ -87,20 +87,20 @@ export const deleteMember = async (req,res,next)=>{
 
         }
         await deleteDoc(memberDoc);
-        res.status(200).send("Member Deleted Successfully!")
+        response.status(200).send("Member Deleted Successfully!")
 
     }catch(error){
-        res.status(400).send(error.message)
+        response.status(400).send(error.message)
     }
 }
 
-export const updateMember = async (req,res,next)=>{
+export const updateMember = async (request,response,next)=>{
     try{
 
-        const {id, ...updatedData} = req.body;
+        const {id, ...updatedData} = request.body;
 
         if( !id || Object.keys(updatedData).length ==0){
-            res.status(400).send("Id and Updated Data required");
+            response.status(400).send("Id and Updated Data required");
         }
 
         console.log(updatedData)
@@ -111,11 +111,11 @@ export const updateMember = async (req,res,next)=>{
         if (memberDocSnap.exists()){
             const previousPhotoURL = memberDocSnap.data().photoURL;
 
-            if (previousPhotoURL && req.files[0]){
+            if (previousPhotoURL && request.files[0]){
                 const fileRef = ref(fireStorageDB, previousPhotoURL);
                 await deleteObject(fileRef);
 
-                const member_photo = req.files[0];
+                const member_photo = request.files[0];
                 const storageRef = ref(fireStorageDB, `members/${updatedData.name}_${Date.now()}_${member_photo.originalname}`);
                 
                 const snapshot = await uploadBytes(storageRef, member_photo.buffer, {
@@ -131,10 +131,10 @@ export const updateMember = async (req,res,next)=>{
 
 
         await updateDoc(memberDoc, updatedData);
-        res.status(200).send(`Member with ID: ${id} has been updated successfully`);
+        response.status(200).send(`Member with ID: ${id} has been updated successfully`);
 
 
     }catch (error){
-        res.status(400).send(error.message);
+        response.status(400).send(error.message);
     }
 }
